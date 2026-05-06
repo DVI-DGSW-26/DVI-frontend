7b250db06d6df5d7d48c082f985056a2753af61f
leeyujin0527 <sksdldbwls0527@gmail.com>
2026-05-06 19:56:22 +0900

    feat : mobile header and tabBar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,9 @@
 import { useMediaQuery } from "../../hooks/useMediaQuery";
 import HeaderWeb from "./Header.web";
+import HeaderMobile from "./Header.mobile";
 
 export default function Header() {
   const isMobile = useMediaQuery("(max-width: 767px)");
 
-  return isMobile ? null : <HeaderWeb />;
+  return isMobile ? <HeaderMobile /> : <HeaderWeb />;
 }
